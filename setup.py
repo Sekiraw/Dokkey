1d7fc3b5711a7388546ea30e3073ab001583c760
Sekiraw <v2020.bohus.peter@gmail.com>
2025-01-12 13:55:15 +0100

    Updated version and author info

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ module = Extension(
 
 setup(
     name="dokkey",
-    version="1.0",
+    version="1.0.1",
     description="A Python module for detecting keyboard events",
+    author="Peter Bohus",
+    author_email="v2020.bohus.peter@gmail.com",
     ext_modules=[module],
 )
