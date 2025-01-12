343c2cd8e00cfc3d3f37ac46b091b70bc64051f1
Sekiraw <v2020.bohus.peter@gmail.com>
2025-01-12 14:21:41 +0100

    Added description

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, Extension
+import pypandoc
 
 module = Extension(
     "dokkey",
@@ -6,10 +7,16 @@ module = Extension(
     libraries=["user32", "kernel32"],  # Windows libraries
 )
 
+description = ""
+try:
+    description=pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    description=open('README.md').read()
+
 setup(
     name="dokkey",
-    version="1.0.2",
-    description="A Python module for detecting keyboard events",
+    version="1.0.3",
+    description=description,
     author="Peter Bohus",
     author_email="v2020.bohus.peter@gmail.com",
     ext_modules=[module],
